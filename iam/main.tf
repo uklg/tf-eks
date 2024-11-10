32bf0fadc354639b99ee3aa7c75f79d3df989928
uklg <rmu@live.co.uk>
2024-11-10 15:24:05 +0000

    ad d in working attachments

--- iam/main.tf
+++ iam/main.tf
@@ -1,13 +1,17 @@
 resource "aws_iam_user_policy_attachment" "attach-1" {
-  #user       = aws_iam_user.user.name
   user = "eks"
   policy_arn = aws_iam_policy.eks-cluster-tf.arn
 }
 
 
 resource "aws_iam_user_policy_attachment" "attach-2" {
-  #user       = aws_iam_user.user.name
   user = "eks"
   policy_arn = aws_iam_policy.eks-lbc-policy-tf.arn
+}
 
+
+
+resource "aws_iam_user_policy_attachment" "attach-3" {
+  user = "eks"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
 }
