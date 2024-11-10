11d1b1ebc547f9c199f962e256cf66681c347dc4
uklg <rmu@live.co.uk>
2024-11-10 15:12:51 +0000

    add in working attachment 2

--- iam/main.tf
+++ iam/main.tf
@@ -3,3 +3,11 @@ resource "aws_iam_user_policy_attachment" "attach-1" {
   user = "eks"
   policy_arn = aws_iam_policy.eks-cluster-tf.arn
 }
+
+
+resource "aws_iam_user_policy_attachment" "attach-2" {
+  #user       = aws_iam_user.user.name
+  user = "eks"
+  policy_arn = aws_iam_policy.eks-lbc-policy-tf.arn
+
+}
