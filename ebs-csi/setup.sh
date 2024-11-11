1f42330d0ccc796353ff1dde92dadc33c2c0dec5
uklg <rmu@live.co.uk>
2024-11-11 12:54:02 +0000

    adding more info

--- ebs-csi/setup.sh
+++ ebs-csi/setup.sh
@@ -11,7 +11,7 @@ export cluster_name=$(terraform output cluster_name| tr -d '"')
 
 
 
-echo check to see if ebi-csi driver is installed in the eks cluster
+echo check to see if ebi-csi driver is installed in the eks cluster. check for ACTIVE status
 
 
 aws eks describe-addon --cluster-name education-eks-OQCBxcyH --addon-name aws-ebs-csi-driver
