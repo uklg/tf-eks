660d0a94219d7c9598e8597f9bbfedcd2ce22c13
uklg <rmu@live.co.uk>
2024-11-11 13:05:16 +0000

    working test

--- ebs-csi/setup.sh
+++ ebs-csi/setup.sh
@@ -24,3 +24,13 @@ kubectl get deploy,ds -l=app.kubernetes.io/name=aws-ebs-csi-driver -n kube-syste
 echo and here
 
 kubectl get po -n kube-system -l 'app in (ebs-csi-controller,ebs-csi-node)'
+
+
+## match to name
+#kubectl get -n kube-system pod/ebs-csi-controller-67c885fcc7-n6ll6 -o jsonpath='{.spec.containers[*].ebs-csi-node-7qjjv}'
+
+
+echo By default, every node is capable of using CSI-based Amazon EBS volumes
+
+kubectl get csinodes
+
