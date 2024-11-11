8de6ba01d7f7c86985ae7347b460e3fc6cf6dddb
uklg <rmu@live.co.uk>
2024-11-11 12:58:04 +0000

    adding tests

--- ebs-csi/setup.sh
+++ ebs-csi/setup.sh
@@ -17,3 +17,10 @@ echo check to see if ebi-csi driver is installed in the eks cluster. check for A
 aws eks describe-addon --cluster-name education-eks-OQCBxcyH --addon-name aws-ebs-csi-driver
 
 
+
+echo check its components are running
+kubectl get deploy,ds -l=app.kubernetes.io/name=aws-ebs-csi-driver -n kube-system
+
+echo and here
+
+kubectl get po -n kube-system -l 'app in (ebs-csi-controller,ebs-csi-node)'
