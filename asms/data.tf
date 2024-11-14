3453afb3c2bf7ceb63f2cf082bd65b7dbcad9b78
uklg <rmu@live.co.uk>
2024-11-14 14:00:34 +0000

    tem p access to module outputs

---
+++ asms/data.tf
@@ -0,0 +1,13 @@
+data "terraform_remote_state" "eks" {
+  backend = "local"
+
+  config = {
+    path = "../terraform.tfstate"
+  }
+}
+
+
+locals {
+  sgId = data.terraform_remote_state.eks.outputs
+}
+
