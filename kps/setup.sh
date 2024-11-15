a6b3c2acccf0a68239621dae8cfe583c1cc9e97f
uklg <rmu@live.co.uk>
2024-11-15 17:22:01 +0000

    test

--- kps/setup.sh
+++ kps/setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 $ helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
-#kubectl create ns monitoring
-kubectl create namespace prometheus
+kubectl create ns monitoring
 
 
-
-helm install stable prometheus-community/kube-prometheus-stack -n prometheus
 
 
 
@@ -18,9 +15,11 @@ helm install stable prometheus-community/kube-prometheus-stack -n prometheus
 echo Install the stack with the custom values.yaml file
 
 
+wget https://raw.githubusercontent.com/prprasad2020/don-cortex-demo/refs/heads/main/Kube-Prometheus-Stack/kube-prometheus-stack-values.yaml
 
+exit
 
-
+helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack -f ~/Documents/kube-prometheus-stack-values.yaml -n monitoring
 
 
 
