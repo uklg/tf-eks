e2e456eddbea0d9d79c0d41e23089b614754efc6
uklg <rmu@live.co.uk>
2024-11-15 17:32:39 +0000

    add in links

--- kps/setup.sh
+++ kps/setup.sh
@@ -12,14 +12,19 @@ kubectl create ns monitoring
 #https://raw.githubusercontent.com/prprasad2020/don-cortex-demo/refs/heads/main/Kube-Prometheus-Stack/kube-prometheus-stack-values.yaml
 
 
-echo Install the stack with the custom values.yaml file
+echo Install the stack with the custom values.yaml file. THis can be updated or referenced if changed
 
 
 wget -O  custom.yaml https://raw.githubusercontent.com/prprasad2020/don-cortex-demo/refs/heads/main/Kube-Prometheus-Stack/kube-prometheus-stack-values.yaml
 
-exit
 
-helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack -f ~/Documents/kube-prometheus-stack-values.yaml -n monitoring
+
+helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack -f custom-moved.json -n monitoring
+
+
+echo dispay the pod status 
+
+kubectl --namespace monitoring get pods -l "release=kube-prometheus-stack"
 
 
 
