99b6960efa79c5815ffb9eb7abcaffd445996a62
uklg <rmu@live.co.uk>
2024-11-20 20:35:57 +0000

    temp spinup

---
+++ kps/setup-no-json-temp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+$ helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+kubectl create ns monitoring
+
+
+
+
+
+
+
+#https://raw.githubusercontent.com/prprasad2020/don-cortex-demo/refs/heads/main/Kube-Prometheus-Stack/kube-prometheus-stack-values.yaml
+
+
+echo Install the stack with the custom values.yaml file. THis can be updated or referenced if changed
+
+
+wget -O  custom.yaml https://raw.githubusercontent.com/prprasad2020/don-cortex-demo/refs/heads/main/Kube-Prometheus-Stack/kube-prometheus-stack-values.yaml
+
+
+
+#helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack -f custom-moved.json -n monitoring
+
+
+helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack -n monitoring
+
+
+
+
+
+echo dispay the pod status 
+
+kubectl --namespace monitoring get pods -l "release=kube-prometheus-stack"
+
+
+
+git pod -A
+
+
