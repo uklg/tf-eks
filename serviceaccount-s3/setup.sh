51e1e42140f31526568723f3e1e9b1763592890d
uklg <rmu@live.co.uk>
2024-12-05 17:47:32 +0000

    test

--- serviceaccount-s3/setup.sh
+++ serviceaccount-s3/setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# echo turn off ebs-csi as this is not prod and we do not need permanent pvs mounted here
+kubectl delete deployments.apps -n kube-system ebs-csi-controller
+
+
+kubectl delete -f deployment.yaml
+
 kubectl apply -f deployment.yaml
 
+
+sleep 1
+kubectl get pod
