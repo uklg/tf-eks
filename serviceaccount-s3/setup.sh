05300fad621c8e94cd3b5e7aa06d10e5274e557b
uklg <rmu@live.co.uk>
2024-12-06 22:21:04 +0000

    working test

--- serviceaccount-s3/setup.sh
+++ serviceaccount-s3/setup.sh
@@ -20,10 +20,33 @@ echo yum install awscli
 aws ls
 
 
-echo An error occurred (AccessDenied) when calling the AssumeRoleWithWebIdentity operation: Not authorized to perform sts:AssumeRoleWithWebIdentity
+echo 'An error occurred (AccessDenied) when calling the AssumeRoleWithWebIdentity operation: Not authorized to perform sts:AssumeRoleWithWebIdentity'
 
 
 echo need to add this in to iam rules
 
 echo kubectl exec -it pod/app-name-7c6c5f46d6-bz257 -- /bin/bash
+
+
+
+
+
+kubectl delete -f infinte-run-deployment.yaml
+
+kubectl apply -f infinte-run-deployment.yaml
+
+pod_name=$(kubectl get pod|grep app-name-infinite|cut -d ' ' -f 1)
+
+
+echo kubectl wait --for=jsonpath='{status.availableReplicas}' deployments/app-name-infinite --timeout=10s
+
+
+echo pod_name=$(kubectl get pod|grep app-name-infinite|cut -d ' ' -f 1)
+
+
+pod_name=$(kubectl get pod|grep app-name-infinite|cut -d ' ' -f 1)
+
+
+echo use this to connect to infinity pod
+echo  kubectl exec -it pod/$pod_name -- /bin/bash
 
